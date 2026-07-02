a425fbbde6a057f00ce1da281a585cabd5cfbf62
Jonathan Marshall <jcmarsha@gmail.com>
2026-07-02 17:08:26 +1200

    adds daily solar

--- data_sources/solar.R
+++ data_sources/solar.R
@@ -1,9 +1,21 @@
 library(tidyverse)
 
 solar <- read_csv("data_sources/raw/5885831_custom_report.csv")
-solar |>
+sol <- solar |>
   mutate(date = mdy_hm(`Date/Time`)) |>
-  mutate(month = month(date, label=TRUE)) |>
-  group_by(month) |>
-  summarise(produced = sum(`Energy Produced (Wh)`)) |>
-  write_csv("data/solar_by_month.csv")
+  mutate(day = yday(date)) |>
+  group_by(day) |>
+  summarise(produced = sum(`Energy Produced (Wh)`))
+
+
+# fit simple smoother by duplication
+foo <- bind_rows(sol, sol, sol, .id='wch') |>
+  mutate(day = (day-1)+365*(as.numeric(wch)-2))
+
+library(mgcv)
+library(broom)
+gam(produced ~ s(day), data=foo) |>
+  augment(foo) |>
+  filter(wch == 2) |>
+  select(produced, smoothed=.fitted, day) |>
+  write_csv("data/solar_by_day.csv")
